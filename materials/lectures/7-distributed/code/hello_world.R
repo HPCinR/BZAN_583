a8b6ba38dcf9b27ac9705b7b9c3ef19102dd99b9
George Ostrouchov <gostrouc@utk.edu>
2024-04-24 13:43:17 -0400

    moved code

--- materials/lectures/7-distributed/code/hello_world.R
+++
@@ -1,9 +0,0 @@
-suppressMessages(library(pbdMPI))
-
-my_rank = comm.rank()
-nranks = comm.size()
-msg = paste0("Hello World! My name is Rank", my_rank,
-             ". We are ", nranks, " identical siblings.")
-cat(msg, "\n")
-
-finalize()
